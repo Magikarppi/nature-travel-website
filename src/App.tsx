9d1e0107fda58223f06086388aca2d79189e252f
Sami H <samih90@gmail.com>
2022-06-21 16:47:57 +0300

    Add NavigationBar with first styles and position

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Tervo Travels</h1>
+        <NavigationBar />
       </div>
     );
   }
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,9 @@
+import { Component, ReactNode } from 'react';
+
+class NavigationBar extends Component {
+  render(): ReactNode {
+    return <div className="NavigationBar"></div>;
+  }
+}
+
+export default NavigationBar;
