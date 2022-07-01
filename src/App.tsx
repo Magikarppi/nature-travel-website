90b8300949fcc757c5db1bc8ec84d7193845b4b2
Sami H <samih90@gmail.com>
2022-07-01 13:58:37 +0300

    Add landing/welcome-image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Component } from 'react';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import syksyHiking from './images/syksyHikingCrop.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Tervo Travels</h1>
-        <NavigationBar />
+        <img
+          src={syksyHiking}
+          alt="syksyinen vaellus"
+          className="LandingImage"
+        />
       </div>
     );
   }
