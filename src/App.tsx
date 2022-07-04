dc437ed8aec9d971396554735de1f8a5190257fa
Sami H <samih90@gmail.com>
2022-07-04 15:09:08 +0300

    Add hiding/showing header based on scrolling direction

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,52 @@ import Fishing from './components/contentSections/Fishing';
 import MyHeader from './components/MyHeader';
 import syksyHiking from './images/syksyHikingCrop.jpg';
 
-class App extends Component {
+interface State {
+  lastScroll: number;
+}
+
+const scrollUp = 'scroll-up';
+const scrollDown = 'scroll-down';
+class App extends Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      lastScroll: 0,
+    };
+  }
+
+  // for hiding header on scroll down and showing on scroll up
+  trackScroll = () => {
+    const body = document.body;
+    const currentScroll = window.scrollY;
+
+    if (currentScroll <= 0) {
+      body.classList.remove(scrollUp);
+    }
+
+    // scrolling down
+    if (
+      currentScroll > this.state.lastScroll &&
+      !body.classList.contains(scrollDown)
+    ) {
+      body.classList.remove(scrollUp);
+      body.classList.add(scrollDown);
+    }
+
+    // scrolling up
+    if (
+      currentScroll < this.state.lastScroll &&
+      !body.classList.contains(scrollUp)
+    ) {
+      body.classList.remove(scrollDown);
+      body.classList.add(scrollUp);
+    }
+
+    this.setState({ lastScroll: currentScroll });
+  };
+
   render() {
+    window.addEventListener('scroll', this.trackScroll);
     return (
       <div className="App">
         <MyHeader />
