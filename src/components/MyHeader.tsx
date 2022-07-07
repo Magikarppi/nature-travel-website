698f7bcf4deeea173c706133a0d8af9584e60983
Sami H <samih90@gmail.com>
2022-07-07 13:13:25 +0300

    Add two sections(links and info) to MyHeader and add initial styles/layout for them

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -4,7 +4,19 @@ class MyHeader extends Component {
   render(): ReactNode {
     return (
       <div className="MyHeader">
-        <h1>Tervo Travels</h1>
+        <div className="HeaderSection">
+          <div className="HeaderSubSectionLeft">
+            <div className="HeaderContentElement">Lisätietoa</div>
+            <div className="HeaderContentElement">Eräpalvelut</div>
+          </div>
+        </div>
+        <div className="HeaderSection">
+          <h1>Tervo Travels</h1>
+        </div>
+        <div className="HeaderSection">
+          <p>TervoTravels@myspace.com</p>
+          <p>+3586738639674</p>
+        </div>
       </div>
     );
   }
