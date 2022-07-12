e6f0f6b5ac5f5846524a69885d08edb925cb5590
Sami H <samih90@gmail.com>
2022-07-12 13:23:54 +0300

    Add initial component for booking

---
+++ src/components/Book.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Book extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Varaussivu</h1>
+      </div>
+    );
+  }
+}
