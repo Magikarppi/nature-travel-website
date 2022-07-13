46a01e574a721bc0626c3b094a1d7fe0f5c02773
Sami H <samih90@gmail.com>
2022-07-13 16:25:00 +0300

    Improve also Booking-page layout/style

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,8 +3,15 @@ import { Component } from 'react';
 export default class Book extends Component {
   render() {
     return (
-      <div>
-        <h1>Varaussivu</h1>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <p>Varaussivu</p>
       </div>
     );
   }
